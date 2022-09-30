61f0975d8ce614a407093f7011107b46e0968d64
Riyan <riyan.afriyanda@students.utdi.ac.id>
2022-10-01 00:33:03 +0700

    first commit

--- app/src/main/java/com/example/rey/data/Datasource.kt
+++ app/src/main/java/com/example/rey/data/Datasource.kt
@@ -3,11 +3,15 @@ import com.example.rey.R
 import com.example.rey.model.Rey
 
 class Datasource {
+//    program diatas ini adalah daftar nilai yang akan ditampilkan
+//    yang akan mengambil data dari strings.xml
     fun loadrey(): List<Rey> {
         return listOf<Rey>(
             Rey(R.string.affirmation1,  R.drawable.image1),
             Rey(R.string.affirmation2, R.drawable.image2),
-            Rey(R.string.affirmation3, R.drawable.image3)
+            Rey(R.string.affirmation3, R.drawable.image3),
+            Rey(R.string.affirmation4, R.drawable.image4),
+            Rey(R.string.affirmation5, R.drawable.image5)
         )
     }
 }
--- app/src/main/java/com/example/rey/model/Rey.kt
+++ app/src/main/java/com/example/rey/model/Rey.kt
@@ -1,4 +1,5 @@
 package com.example.rey.model
+//program dibawah ini akan menghapus juka terjadi eror
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
